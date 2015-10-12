c5810aebcd260c04a34f6f3e46e6595ec3aa2507
Guilherme <guinatal.mello@gmail.com>
2015-10-12 22:08:54 +0100

    Versions
    Chrome 42+ or 43+ ? 
    Opera 29+ or 30+ ?
    
    https://github.com/zenorocha/clipboard.js

--- src/queryCommandSupported.js
+++ src/queryCommandSupported.js
@@ -4,25 +4,19 @@ var _queryCommandSupported = document.queryCommandSupported;
 
 document.queryCommandSupported = function(cmd) {
     if (cmd === 'copy' || cmd === 'cut') {
-        if (bowser.chrome) {
-            if (bowser.version >= 43) {
-                return true;
-            }
-
-            return false;
+        if (bowser.chrome && bowser.version >= 42) {
+            return true;
         }
 
-        if (bowser.opera) {
-            if (bowser.version >= 30) {
-                return true;
-            }
-
-            return false;
+        if (bowser.opera && bowser.version >= 29) {
+            return true;
         }
 
-        if (bowser.firefox && bowser.version < 41) {
-            return false;
+        if (bowser.firefox && bowser.version >= 41) {
+            return true;
         }
+        
+        return false;
     }
 
     return _queryCommandSupported.apply(this, arguments);
